19beb6d489fc5e94207cd1141b91e5ec6654ea5f
brycegaylord10 <56646667+brycegaylord10@users.noreply.github.com>
2019-11-04 11:10:00 -0600

    Delete play.js

--- CIT Project/assets/css/play.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-$('#play').on('click', function(e) {
-	e.preventDefault();
-	$("#player")[0].src += "?autoplay=1";
-	$('#player').show();
-	$('#video-cover').hide();
-	$('#play').hide();
-})
